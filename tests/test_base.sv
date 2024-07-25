7eeb79a1175c1d3a1fbaf6cb2d22964303025ec1
Leader <fracing@fia.com>
2024-07-25 11:41:24 +0200

    test base updated

---
+++ tests/test_base.sv
@@ -0,0 +1,17 @@
+class base_test;
+  gpio1 = 1;
+  wdog = 0;
+  ldo1_volt = 5'b1_0010;
+  sw1_voly = 5'b1_0101;
+
+  $display("creating merge fault");
+  $display("added new line");
+  pwm_seq pwm_period_swp;
+  wdog_seq wdog_error_seq;
+  $display("merge fault");
+
+  pwm_period_seq.start(m_seqr);
+  wdog_error_seq.start(m_seqr);
+  $display("modified");
+  $display"chekcing git pull feature");
+endclass
